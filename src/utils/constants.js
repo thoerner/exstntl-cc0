e1a8780caec269b1080ed56d3cf4d7c5eebc56e9
Tim Hoerner <thoerner@gmail.com>
2022-08-07 22:07:19 -0400

    fixed redirect link

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const METAMASK_REDIRECT_URL = 'https://metamask.app.link/dapp/www.web3point69.com';
+export const METAMASK_REDIRECT_URL = 'https://metamask.app.link/dapp/www.cc0mune.xyz';
 
 export const CONTRACT_ADDRESS = '0x8130685D6FE8f15d10b4f8d4d25b89691010E07c';
 
